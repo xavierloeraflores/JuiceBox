0cd97e69f2dbc715901c660666bcb298b95a765d
Xavier Loera Flores <x.loera.tech@gmail.com>
2021-09-18 13:08:40 -0700

    setup

--- db/index.js
+++ db/index.js
@@ -1,6 +1,9 @@
 const { Client } = require('pg') // imports the pg module
 
-const client = new Client(process.env.DATABASE_URL || 'postgres://localhost:5432/juicebox-dev');
+const client = new Client({
+  connectionString: process.env.DATABASE_URL || 'postgres://localhost:5432/juicebox-dev',
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
+});
 
 /**
  * USER Methods
